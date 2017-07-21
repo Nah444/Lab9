361789b7a2501d6c18d56103f00331d4cd2d6ee5
Nicholos Hatcher <nicholos.hatcher@gmail.com>
2017-07-21 09:59:35 -0400

    first commit

---
+++ src/Cars.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Grand Circus Student on 7/20/2017.
+ */
+import java.util.Scanner;
+
+public class Cars {
+
+    private String carModel;
+    private String carMake;
+    private int year;
+    private double price;
+
+    public Cars() {
+
+        make = "Ford";
+        model = "Focus";
+        year = 2001;
+        price = 3000.00;
+    }
+
+    public Cars (String makeOfCar, String modelOfCar, int carYear, double carPrice){
+
+        make = makeOfCar;
+        model = modelOfCar;
+        year = carYear;
+        double = carPrice;
+    }
+
+}
+
+
