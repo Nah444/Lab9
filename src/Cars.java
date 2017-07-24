698ac3a7a2866aca857ceaf079b65fedc156b095
Nick Hatcher <nicholos.hatcher@gmail.com>
2017-07-24 13:09:00 -0400

    Update Cars.java

--- src/Cars.java
+++ src/Cars.java
@@ -1,31 +1,78 @@
-/**
- * Created by Grand Circus Student on 7/20/2017.
- */
 import java.util.Scanner;
 
-public class Cars {
+/**
+ * Created by Jared on 7/20/17.
+ */
+public class CarApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numCars = 0;
+        String contin = "yes";
 
-    private String carModel;
-    private String carMake;
-    private int year;
-    private double price;
+        System.out.println("Welcome to Grand Circus Motors admin console!");
 
-    public Cars() {
+        do {
+            System.out.println("How many cars are you entering: ");
+            numCars = scan.nextInt();
+            System.out.println(numCars + " cars? Okayyyyy");
 
-        make = "Ford";
-        model = "Focus";
-        year = 2001;
-        price = 3000.00;
-    }
+            Car[] cars = new Car[numCars];
+            String mk = "SomeKindaCar";
+            String ml = "Pinto";
+            int y = 0;
+            double pc = 0.00;
 
-    public Cars (String makeOfCar, String modelOfCar, int carYear, double carPrice){
+            for (int i = 0; i < numCars; i++) {
+                scan.nextLine();
+                System.out.println("Car " + (i + 1) + "");
+                System.out.println("Enter make: ");
+                mk = scan.nextLine();
+                System.out.println(" ");
+                System.out.println("Enter model: ");
+                ml = scan.nextLine();
+                System.out.println();
+                System.out.println("Enter year: ");
+                y = scan.nextInt();
 
-        make = makeOfCar;
-        model = modelOfCar;
-        year = carYear;
-        double = carPrice;
+
+                if (y > 2018 || y < 1900) {
+                    System.out.println("Please enter a year betweeen 1900 and 2018: ");
+                    y = scan.nextInt();
+                }
+                System.out.println();
+                System.out.println("Enter price: ");
+                pc = scan.nextDouble();
+                //checks to make sure price is realistic.
+
+                if (pc < 99.00) {
+                    System.out.println("That's a cheap car! If it works," +
+                            " I'll buy it from you right now.");
+
+                } else if (pc > 1000000.00) {
+                    System.out.println("That's one spicy meatball!"); }
+                System.out.println();
+                Car car = new Car(mk, ml, y, pc);
+                cars[i] = car;
+            }
+
+            System.out.println("...");
+            System.out.println("Current Inventory");
+            //extended for loop to get specific information from cars[]
+            //Print out a table of the cars at the end
+
+            for (Car car : cars) {
+                System.out.println(car.getMake() + " " + car.getModel() + " " + car.getYear() + " " + "$" + car.getPrice());
+            }
+            System.out.println();
+            //allow user to continue
+            System.out.println("Enter new set of cars?");
+            scan.nextLine();
+            contin = scan.nextLine();
+
+        } while (contin.contains("Y") || contin.contains("y"));
+        //final greeting
+        System.out.println("Peace.");
+
     }
 
 }
-
-
